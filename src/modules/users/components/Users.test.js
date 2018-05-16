7cde73a299ee3b0aebcfc3b11a29995beb0d0b65
Panchenko <anton.panchenko@sigmaukraine.com>
2018-05-16 14:59:52 +0300

    toMatchSnapshot

---
+++ src/modules/users/components/Users.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Users from './Users';
+
+const props = {
+  removeUser: () => undefined,
+  loadUsersList: () => undefined,
+  users: [
+    { name: 'Bob3', id: 'sfdsdfsdf' },
+    { name: 'Bob2', id: 'dfgwerwrd' },
+    { name: 'Bob1', id: 's234sfsdg' },
+  ],
+};
+
+describe('<Users />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Users {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
